b49bf27b2b7e1a79fef560fd6a4a034251bc77c1
Bùi Đức Vinh <65385174+buivinhk1@users.noreply.github.com>
2022-10-23 10:03:31 +0700

    test

--- DACN/Controllers/HomeController.cs
+++ DACN/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DACN.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application descriptionsdgg page.";
 
             return View();
         }
